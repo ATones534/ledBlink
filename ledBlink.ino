e2f6e12a5e789953e3bdd8ce84270dc4f5de9a5e
ATones534 <95202984+ATones534@users.noreply.github.com>
2021-11-29 10:54:26 -0500

    Added Button
    This is finished blink code with a added button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,12 +1,21 @@
 int ledPin = 32;
+int ledPin2 = 30;
+int buttonPin = 29;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  
+  pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (digitalRead(buttonPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+    delay(500);
+  }
 }
